60cbecb1a3a154c53bd9b215fed6612b2962512e
boogity <william@DOIT-WILLIAM.dsa.reldom.tamu.edu>
2019-05-31 11:30:58 -0500

    finishes disable and ToBeDeleted functions, consider changing logic to only move TBD machines who have been disabled for 60 days

---
+++ InactiveComputerRemoval.ps1
@@ -0,0 +1,53 @@
+﻿Import-Module activedirectory
+
+$daysInactiveDisable = 120
+$daysInactiveDelete = 180
+$hash_lastLogonTimestamp = @{Name="LastLogonTimeStamp";Expression={([datetime]::FromFileTime($_.LastLogonTimeStamp))}} #User Readable LastLogonTimeStamp from https://blogs.technet.microsoft.com/chadcox/2017/08/18/active-directory-powershell-quick-tip-lastlogontimestamp-and-pwdlastset/
+$today = Get-Date
+$disableTime = ($today).Adddays(-($daysInactiveDisable))
+$disableList = @()
+$ToBeDeletedList = @()
+$deletedOU = "OU=Computers to be Deleted,OU=IT Dumpster,OU=Student Affairs,DC=dsa,DC=reldom,DC=tamu,DC=edu" #Move PCs to Computers to be deleted OU on disable
+
+#Check for computers currently enabled but last logon date >120 days -> disable and add description with date/time
+function DisableComputers
+{
+    $description = "Computer disabled for inactivity on $today"
+    
+    #Enabled computers with last logon date >120 days
+    $disableList = Get-ADComputer -Filter {LastLogonTimeStamp -lt $disableTime} -SearchBase "OU=Student Affairs,DC=dsa,DC=reldom,DC=tamu,DC=edu" -resultSetSize $null `
+    -Properties Name, OperatingSystem, SamAccountName, DistinguishedName, LastLogonTimeStamp, Enabled | Where { $_.Enabled -eq $true}
+    
+    #Iterate through array, disable and add date/time to description
+    foreach ($Computer in $disableList) 
+    {
+
+       Set-ADComputer ($Computer).Name -Description $description -Enabled $false -WhatIf
+    }
+    $disableList | select Name, DistinguishedName, $hash_lastLogonTimestamp, Enabled | Export-Csv "C:\TEMP\disabledComputers.csv"
+}
+
+#Check for disabled computers with last logon date >180 days -> Move to Computers To Be Deleted OU
+function ComputersToBeDeleted
+{
+    $description = "Computer disabled for inactivity on $today"
+    
+    #Create array of disabled computers with last logon date >180 days 
+    $ToBeDeletedList = Get-ADComputer -Filter {LastLogonTimeStamp -lt $deleteTime} -SearchBase "OU=Student Affairs,DC=dsa,DC=reldom,DC=tamu,DC=edu" -resultSetSize $null `
+    -Properties Name, OperatingSystem, SamAccountName, DistinguishedName, LastLogonTimeStamp, Enabled | Where { $_.Enabled -eq $false}
+
+    #Iterate through array, move to To Be Deleted OU
+    foreach ($Computer in $ToBeDeletedList) 
+    {
+        Move-ADObject -Identity ($Computer).DistinguishedName -TargetPath $deletedOU -WhatIf
+    }
+    $ToBeDeletedList | select Name, DistinguishedName, $hash_lastLogonTimestamp, Enabled | Export-Csv "C:\TEMP\toBeDeletedComputers.csv"
+}
+function main
+{
+    ComputersToBeDeleted
+    DisableComputers
+   
+}
+
+main
---
+++ README.md
@@ -0,0 +1,5 @@
+Automatically disable computers in AD's Student Affairs OU after 120 days with no logon and add a description showing the disabled date. After 180 days with no activity or re-enabling (+60 days from the date of initial disable, logic checks to verify only looks for disabled machines) it will move these computers to the To Be Deleted OU. 
+
+The DisableComputers function of the script will leave machines in the same OU but disable them and add the description, "Computer disabled for inactivity on $today" to them. See disabledComputers.xlsx for the list of computers that will be disabled on our first run of the script. 
+
+The ComputersToBeDeleted function of the script will *not* delete any objects from AD, at least for the first several months, if ever. Instead the current plan is to automate moving them to the "To Be Deleted OU" and having a level 2 look them over once a week/month/some-interval-to-be-determined and manually delete them from AD after verifying there are no servers or other false positives.
